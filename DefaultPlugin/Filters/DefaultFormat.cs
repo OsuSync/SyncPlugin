abc4c453ecdac48497dea01ab8b83e08049bf8ca
Deliay <admin@remiliascarlet.com>
2017-09-08 09:29:55 +0800

    Offical plugins

---
+++ DefaultPlugin/Filters/DefaultFormat.cs
@@ -0,0 +1,17 @@
+﻿using Sync.MessageFilter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultPlugin.Filters
+{
+    class DefaultFormat : IFilter, ISourceDanmaku, ISourceClient
+    {
+        public void onMsg(ref IMessageBase msg)
+        {
+            msg.User = new Sync.Tools.StringElement("<", msg.User.RawText, ">: ");
+        }
+    }
+}
