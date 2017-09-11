5257b9692f43f28fb2ba1dc2c35002bf132cc476
Mikira Sora <mikirasora0409@126.com>
2017-09-11 16:58:48 +0800

    Fixed NowPlaying

--- NowPlaying/NowPlaying.cs
+++ NowPlaying/NowPlaying.cs
@@ -7,28 +7,145 @@ using Sync;
 using Sync.Command;
 using Sync.MessageFilter;
 using System.Threading.Tasks;
+using Sync.Tools;
+using System.IO;
+using osu_database_reader;
+using System.Diagnostics;
+using System.Threading;
 using static Sync.Plugins.PluginEvents;
+
 namespace NowPlaying
 {
-    public class NowPlaying : Plugin, IFilter, ISourceDanmaku
+    public class NowPlaying : Plugin, IFilter, ISourceDanmaku,IConfigurable
     {
         private MessageDispatcher MainMessager = null;
         private MSNHandler handler = null;
         private OSUStatus osuStat = new OSUStatus();
 
+        PluginConfigurationManager config;
+
+        public static ConfigurationElement OsuFolderPath { get; set; } = "";
+        public static ConfigurationElement EnableAdvanceFeature { get; set; } = "0";
+
+        private bool supportAdvanceInfo { get => CurrentBeatmapList != null; }
+        Stopwatch sw = new Stopwatch();
+
+        List<BeatmapEntry> CurrentBeatmapList;
+        FileSystemWatcher CurrentOsuFilesWatcher;
+        BeatmapEntry CurrentPlayingBeatmap;
+
+        public delegate void OnCurrentPlayingBeatmapChangedFunc(BeatmapEntry new_beatmap);
+        public event OnCurrentPlayingBeatmapChangedFunc OnCurrentPlayingBeatmapChangedEvent;
+
         public NowPlaying() : base("Now Playing", "Deliay")
         {
         }
 
+        private void InitAdvance()
+        {
+            try
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+                if (string.IsNullOrWhiteSpace(OsuFolderPath))
+                {
+                    IO.CurrentIO.WriteColor($"[NowPlaying]未设置osu文件夹路径，将自行搜寻当前运行中的osu程序来自动设置", ConsoleColor.Yellow);
+
+                    var processes = Process.GetProcessesByName(@"osu!");
+                    if (processes.Length != 0)
+                    {
+                        OsuFolderPath = processes[0].MainModule.FileName.Replace(@"osu!.exe", string.Empty);
+                        IO.CurrentIO.WriteColor($"[NowPlaying]Found osu!.exe ,Get osu folder:{OsuFolderPath}", ConsoleColor.Green);
+                    }
+                    else
+                    {
+                        IO.CurrentIO.WriteColor($"[NowPlaying]未设置osu文件夹路径，也没运行中的osu程序，无法使用此插件其他高级功能，请设置好路径并重新启动osuSync才能继续使用", ConsoleColor.Red);
+                    }
+                }
+
+                var currentDatabase = OsuDb.Read(OsuFolderPath + "osu!.db");
+                Console.WriteLine($"========={sw.ElapsedMilliseconds}=============");
+                sw.Stop();
+                CurrentBeatmapList = currentDatabase.Beatmaps;
+                CurrentOsuFilesWatcher = new FileSystemWatcher(OsuFolderPath + @"Songs", "*.osu");
+                CurrentOsuFilesWatcher.EnableRaisingEvents = true;
+                CurrentOsuFilesWatcher.IncludeSubdirectories = true;
+                CurrentOsuFilesWatcher.Changed += CurrentOsuFilesWatcher_Changed;
+            }
+            catch (Exception e)
+            {
+                IO.CurrentIO.WriteColor($"[NowPlaying]trying support advance features failed!,{e.Message}", ConsoleColor.Yellow);
+                CurrentBeatmapList = null;
+            }
+        }
+
+        private void CurrentOsuFilesWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            string content = "";
+
+            Thread.Sleep(10);
+
+            using (StreamReader reader = new StreamReader(File.Open(e.FullPath, FileMode.Open, FileAccess.Read, FileShare.Read)))
+            {
+                content = reader.ReadToEnd();
+            }
+
+            BeatmapEntry beatmap = OsuFileParser.ParseText(content);
+
+            if (beatmap == null)
+            {
+                return;
+            }
+
+            var select_beatmaps = CurrentBeatmapList.AsParallel().Where((enum_beatmap) =>
+            {
+                if (((enum_beatmap.Title.Trim() == beatmap.Title.Trim())) && enum_beatmap.Difficulty == beatmap.Difficulty && ((enum_beatmap.Artist.Trim() == beatmap.Artist.Trim())))
+                    return true;
+                return false;
+            });
+
+            if (select_beatmaps.Count() != 0)
+            {
+                CurrentBeatmapList.Remove(select_beatmaps.First());
+            }
+
+            CurrentBeatmapList.Add(beatmap);
+
+#if DEBUG
+
+            IO.CurrentIO.WriteColor($"file {e.Name} was modified/created.beatmap :{beatmap.ArtistUnicode ?? beatmap.Artist} - {beatmap.TitleUnicode ?? beatmap.Title}", ConsoleColor.Green);
+
+#endif
+        }
+        
         public override void OnEnable()
         {
             base.EventBus.BindEvent<InitFilterEvent>((filter) => filter.Filters.AddFilter(this));
             base.EventBus.BindEvent<LoadCompleteEvent>(evt => MainMessager = evt.Host.Messages);
+            base.EventBus.BindEvent<InitPluginEvent>(NowPlaying_onInitPlugin);
             handler = new MSNHandler();
 
             Sync.Tools.IO.CurrentIO.WriteColor(Name + " By " + Author, ConsoleColor.DarkCyan);
             //绑定NowPlayingEvents这个Dispatcher的StatusChangeEvent事件
             NowPlayingEvents.Instance.BindEvent<StatusChangeEvent>(OnOSUStatusChange);
+        }
+
+        private void NowPlaying_onInitPlugin(InitPluginEvent e)
+        {
+            Sync.Tools.IO.CurrentIO.WriteColor(Name + " By " + Author, ConsoleColor.DarkCyan);
+
+            config = new PluginConfigurationManager(this);
+            config.AddItem(this);
+
+            if (((string)EnableAdvanceFeature).Trim()=="1")
+            {
+                InitAdvance();
+
+                if (supportAdvanceInfo)
+                    //handler.registerCallbackp => new Task<bool>(OnOsuStatusAdvanceChange, p));
+                    NowPlayingEvents.Instance.BindEvent<StatusChangeEvent>(OnOsuStatusAdvanceChange);
+            }
         }
 
         private void OnOSUStatusChange(StatusChangeEvent @event)
@@ -41,32 +158,71 @@ namespace NowPlaying
 
         public void onMsg(ref IMessageBase msg)
         {
-            if (msg.Message.RawText.Equals("?np"))
-            {
-                msg.Cancel = true;
-                string strMsg = string.Empty;
-                if (osuStat.status == "Playing")
-                {
-                    strMsg = "玩";
-                }
-                else if (osuStat.status == "Editing")
-                {
-                    strMsg = "做";
-                }
-                else //include  Listening
-                {
-                    strMsg = "听";
-                }
-                if (osuStat.title.Length > 17)
-                {
-                    MainMessager.onIRC(SyncHost.Instance.ClientWrapper.Client.NickName, "我在" + strMsg + osuStat.title.Substring(0, 14) + "...");
-                }
-                else
-                {
-                    MainMessager.onIRC(SyncHost.Instance.ClientWrapper.Client.NickName, "我在" + strMsg + osuStat.title);
-                }
-            }
+            if (!msg.Message.RawText.StartsWith("?np"))
+                return;
 
+            msg.Cancel = true;
+            string param = msg.Message.RawText.Replace("?np", string.Empty).Trim();
+            switch (param)
+            {
+                case "":
+                    SendCurrentStatus();
+                    break;
+
+                case "-setid":
+                case "-sid":
+                    SendCurrentBeatmapSetID();
+                    break;
+
+                case "-hp":
+                    SendCurrentBeatmapHP();
+                    break;
+
+                case "-od":
+                    SendCurrentBeatmapOD();
+                    break;
+
+                case "-cs":
+                    SendCurrentBeatmapCS();
+                    break;
+
+                case "-ar":
+                    SendCurrentBeatmapAR();
+                    break;
+
+                case "-id":
+                    SendCurrentBeatmapID();
+                    break;
+
+                default:
+                    SendRawMessage( $"无效的命令\"{param}\"");
+                    break;
+            }
+        }
+
+        private void SendCurrentStatus()
+        {
+            string strMsg = string.Empty;
+            if (osuStat.status == "Playing")
+            {
+                strMsg = "玩";
+            }
+            else if (osuStat.status == "Editing")
+            {
+                strMsg = "做";
+            }
+            else //include  Listening
+            {
+                strMsg = "听";
+            }
+            if (osuStat.title.Length > 17)
+            {
+                SendRawMessage( "我在" + strMsg + osuStat.title.Substring(0, 14) + "...");
+            }
+            else
+            {
+                SendRawMessage( "我在" + strMsg + osuStat.title);
+            }
         }
 
         [Obsolete("Replace with EventBus", true)]
@@ -74,5 +230,69 @@ namespace NowPlaying
         {
             ((IMSNHandler)handler).registerCallback(callback);
         }
+
+        private void OnOsuStatusAdvanceChange(StatusChangeEvent stat)
+        {
+            if (!supportAdvanceInfo)
+                return;
+
+            var currentOsuStat = stat.CurrentStatus;
+
+            sw.Reset();
+            sw.Start();
+
+            var query_result = (!string.IsNullOrWhiteSpace(currentOsuStat.title ?? currentOsuStat.artist)) ? CurrentBeatmapList.AsParallel().Where(
+                (beatmap) => (((currentOsuStat.title.Trim() == beatmap.TitleUnicode.Trim()) || (currentOsuStat.title.Trim() == beatmap.Title.Trim())) && currentOsuStat.diff == beatmap.Difficulty && ((currentOsuStat.artist.Trim() == beatmap.ArtistUnicode.Trim()) || (currentOsuStat.artist.Trim() == beatmap.Artist.Trim())))
+                ) : null;
+
+            var temp_beatmap = CurrentPlayingBeatmap;
+            CurrentPlayingBeatmap = null;
+
+            if (query_result != null && query_result.Count() != 0)
+            {
+                IO.CurrentIO.WriteColor($"query_result count:{query_result.Count()}\ttime={sw.ElapsedMilliseconds}ms\t", ConsoleColor.Cyan);
+                BeatmapEntry beatmap = query_result.First();
+                var title = string.IsNullOrWhiteSpace(beatmap.TitleUnicode) ? beatmap.Title : beatmap.TitleUnicode;
+                var artist = string.IsNullOrWhiteSpace(beatmap.ArtistUnicode) ? beatmap.Artist : beatmap.ArtistUnicode;
+                IO.CurrentIO.WriteColor($"[{beatmap.SongSource}]({artist}) - {title}[{beatmap.Difficulty}](AR/HP/OD/CS:{beatmap.DiffAR}/{beatmap.DiffHP}/{beatmap.DiffOD}/{beatmap.DiffCS})", ConsoleColor.Cyan);
+                CurrentPlayingBeatmap = beatmap;
+            }
+
+            sw.Stop();
+
+            if (temp_beatmap != CurrentPlayingBeatmap)
+            {
+                OnCurrentPlayingBeatmapChangedEvent?.Invoke(CurrentPlayingBeatmap);
+            }
+
+            return;
+        }
+
+        public void onConfigurationLoad()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void onConfigurationSave()
+        {
+            throw new NotImplementedException();
+        }
+        public void SendCurrentBeatmapSetID() => SendRawMessage (CurrentPlayingBeatmap!= null ? $"当前铺面SetID:{CurrentPlayingBeatmap.BeatmapSetId}" : $"咕咕咕,当前并没打任何图");
+
+        public void SendCurrentBeatmapID() => SendRawMessage(CurrentPlayingBeatmap != null ? $"当前铺面ID:{CurrentPlayingBeatmap.BeatmapId}" : $"咕咕咕,当前并没打任何图");
+
+        public void SendCurrentBeatmapAR() => SendRawMessage(CurrentPlayingBeatmap != null ? $"当前铺面AR:{CurrentPlayingBeatmap.DiffAR}" : $"咕咕咕,当前并没打任何图");
+
+        public void SendCurrentBeatmapHP() => SendRawMessage(CurrentPlayingBeatmap != null ? $"当前铺面HP:{CurrentPlayingBeatmap.DiffHP}" : $"咕咕咕,当前并没打任何图");
+
+        public void SendCurrentBeatmapCS() => SendRawMessage(CurrentPlayingBeatmap != null ? $"当前铺面CS:{CurrentPlayingBeatmap.DiffCS}" : $"咕咕咕,当前并没打任何图");
+
+        public void SendCurrentBeatmapOD() => SendRawMessage(CurrentPlayingBeatmap != null ? $"当前铺面OD:{CurrentPlayingBeatmap.DiffOD}" : $"咕咕咕,当前并没打任何图");
+
+        public void SendRawMessage(string message)
+        {
+            SyncHost.Instance.Messages.RaiseMessage<ISourceDanmaku>(new DanmakuMessage() {Message=message});
+        }
+
     }
 }
---
+++ NowPlaying/OsuFileParser.cs
@@ -0,0 +1,73 @@
+﻿using osu_database_reader;
+using Sync.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NowPlaying
+{
+    public static class OsuFileParser
+    {
+        public static BeatmapEntry ParseText(string content)
+        {
+            BeatmapEntry entry = new BeatmapEntry();
+
+            var parser_data = PickValues(ref content);
+
+            try
+            {
+                entry.Artist = TryGetValue(parser_data, "Artist");
+                entry.ArtistUnicode = TryGetValue(parser_data, "ArtistUnicode");
+                entry.Title = TryGetValue(parser_data, "Title");
+                entry.TitleUnicode = TryGetValue(parser_data, "TitleUnicode");
+                entry.Creator = TryGetValue(parser_data, "Creator");
+                entry.SongSource = TryGetValue(parser_data, "Source");
+                entry.SongTags = TryGetValue(parser_data, "Tags");
+
+                entry.BeatmapId = int.Parse(TryGetValue(parser_data, "BeatmapID", "-1"));
+
+                entry.BeatmapSetId = int.Parse(TryGetValue(parser_data, "BeatmapSetID", "-1"));
+
+                entry.Difficulty = TryGetValue(parser_data, "Version");
+                entry.DiffAR = float.Parse(TryGetValue(parser_data, "ApproachRate", "-1.0"));
+                entry.DiffOD = float.Parse(TryGetValue(parser_data, "OverallDifficulty", "-1.0"));
+                entry.DiffCS = float.Parse(TryGetValue(parser_data, "CircleSize", "-1.0"));
+                entry.DiffHP = float.Parse(TryGetValue(parser_data, "HPDrainRate", "-1.0"));
+
+                return entry;
+            }
+            catch (Exception e)
+            {
+                IO.CurrentIO.WriteColor($"Parse error:{e.Message},will pass this beatmap", ConsoleColor.Yellow);
+            }
+
+            return null;
+        }
+
+        static string TryGetValue(Dictionary<string, string> map, string key, string default_value = "")
+        {
+            if (map.ContainsKey(key))
+            {
+                return map[key];
+            }
+
+            return default_value;
+        }
+
+        public static Dictionary<string, string> PickValues(ref string content)
+        {
+            MatchCollection result = Regex.Matches(content, $@"^(\w+):(.*)$", RegexOptions.Multiline);
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+
+            foreach (Match match in result)
+            {
+                dic.Add(match.Groups[1].Value, match.Groups[2].Value.Replace("\r", string.Empty));
+            }
+
+            return dic;
+        }
+    }
+}
