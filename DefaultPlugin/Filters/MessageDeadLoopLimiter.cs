41b2d7462ece67ace47e25a17f82de91cfaf86ac
Deliay <admin@remiliascarlet.com>
2017-12-14 15:21:04 +0800

    Reslove message dead loop when use gameplay osu account as a bot

---
+++ DefaultPlugin/Filters/MessageDeadLoopLimiter.cs
@@ -0,0 +1,23 @@
+﻿using DefaultPlugin.Clients;
+using Sync.MessageFilter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultPlugin.Filters
+{
+    class MessageDeadLoopLimiter : IFilter, ISourceClient
+    {
+        public void onMsg(ref IMessageBase msg)
+        {
+            if(DefaultPlugin.MainClient.Client is DirectOSUIRCBot client &&
+                DirectOSUIRCBot.IRCBotName == DirectOSUIRCBot.IRCNick &&
+                msg.User.RawText == DirectOSUIRCBot.IRCBotName)
+            {
+                msg.Cancel = true;
+            }
+        }
+    }
+}
