1ad45576aee87a2539969c3ee4490e708c00477c
KedamaOvO <moe@mao-yu.net>
2018-05-23 12:23:02 +0800

    remove --delay-initialize=3

--- DefaultPlugin/Commands/BaseCommand.cs
+++ DefaultPlugin/Commands/BaseCommand.cs
@@ -124,7 +124,7 @@ namespace DefaultPlugin.Commands
 
         private bool restart(Arguments arg)
         {
-            Process.Start(Assembly.GetEntryAssembly().Location, "--delay-initialize=3");
+            Process.Start(Assembly.GetEntryAssembly().Location);
             Environment.Exit(0);
             return true;
         }
