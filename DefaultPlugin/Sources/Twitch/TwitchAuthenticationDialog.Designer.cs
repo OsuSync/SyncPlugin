c1f7cc93d9ffd36552b51544662a26d5d80d37bf
Deliay <admin@remiliascarlet.com>
2018-06-24 17:37:36 +0800

    v2.18

--- DefaultPlugin/Sources/Twitch/TwitchAuthenticationDialog.Designer.cs
+++ DefaultPlugin/Sources/Twitch/TwitchAuthenticationDialog.Designer.cs
@@ -43,32 +43,32 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(495, 22);
+            this.label1.Size = new System.Drawing.Size(154, 22);
             this.label1.TabIndex = 0;
-            this.label1.Text = "获取Twitch直播间的评论(message)需要OAuth。请先输入OAuth:";
+            this.label1.Text = "Twitch OAuth key";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.linkLabel1.ForeColor = System.Drawing.SystemColors.ControlText;
             this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.linkLabel1.Location = new System.Drawing.Point(12, 61);
+            this.linkLabel1.Location = new System.Drawing.Point(12, 66);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(134, 19);
+            this.linkLabel1.Size = new System.Drawing.Size(177, 19);
             this.linkLabel1.TabIndex = 1;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "我该如何获取OAuth?";
+            this.linkLabel1.Text = "How to get my OAuth key?";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("宋体", 11F);
-            this.textBox1.Location = new System.Drawing.Point(72, 34);
+            this.textBox1.Font = new System.Drawing.Font("SimSun", 11F);
+            this.textBox1.Location = new System.Drawing.Point(72, 37);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(435, 24);
             this.textBox1.TabIndex = 2;
@@ -77,8 +77,8 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("微软雅黑", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(12, 37);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(55, 20);
             this.label2.TabIndex = 3;
@@ -86,55 +86,55 @@
             // 
             // OK
             // 
-            this.OK.Location = new System.Drawing.Point(16, 241);
+            this.OK.Location = new System.Drawing.Point(16, 261);
             this.OK.Name = "OK";
-            this.OK.Size = new System.Drawing.Size(91, 31);
+            this.OK.Size = new System.Drawing.Size(91, 34);
             this.OK.TabIndex = 4;
-            this.OK.Text = "确认";
+            this.OK.Text = "Apply";
             this.OK.UseVisualStyleBackColor = true;
             this.OK.Click += new System.EventHandler(this.button1_Click);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(16, 115);
+            this.checkBox1.Location = new System.Drawing.Point(16, 125);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(408, 16);
+            this.checkBox1.Size = new System.Drawing.Size(130, 17);
             this.checkBox1.TabIndex = 6;
-            this.checkBox1.Text = "想要用自己的ClientID支持在线人数更新提示(不勾则使用默认ClientID)";
+            this.checkBox1.Text = "Using custom ClientID";
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(12, 134);
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(12, 145);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(384, 22);
+            this.label3.Size = new System.Drawing.Size(462, 22);
             this.label3.TabIndex = 7;
-            this.label3.Text = "若想支持直播间在线人数实时跟新，请输入ClientID:";
+            this.label3.Text = "Custome ClientID could refresh online counter realtime.";
             this.label3.Visible = false;
             // 
             // linkLabel2
             // 
             this.linkLabel2.AutoSize = true;
-            this.linkLabel2.Font = new System.Drawing.Font("微软雅黑", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.linkLabel2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.linkLabel2.ForeColor = System.Drawing.SystemColors.ControlText;
             this.linkLabel2.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.linkLabel2.Location = new System.Drawing.Point(12, 190);
+            this.linkLabel2.Location = new System.Drawing.Point(12, 206);
             this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(142, 19);
+            this.linkLabel2.Size = new System.Drawing.Size(155, 19);
             this.linkLabel2.TabIndex = 8;
             this.linkLabel2.TabStop = true;
-            this.linkLabel2.Text = "我该如何获取ClientID?";
+            this.linkLabel2.Text = "Hot to get my ClientID?";
             this.linkLabel2.Visible = false;
             this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
             // 
             // textBox2
             // 
-            this.textBox2.Font = new System.Drawing.Font("宋体", 11F);
-            this.textBox2.Location = new System.Drawing.Point(88, 163);
+            this.textBox2.Font = new System.Drawing.Font("SimSun", 11F);
+            this.textBox2.Location = new System.Drawing.Point(88, 177);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(419, 24);
             this.textBox2.TabIndex = 9;
@@ -143,8 +143,8 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("微软雅黑", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(12, 163);
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(12, 177);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(70, 20);
             this.label4.TabIndex = 10;
@@ -153,9 +153,9 @@
             // 
             // TwitchAuthenticationDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 284);
+            this.ClientSize = new System.Drawing.Size(523, 308);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.linkLabel2);
