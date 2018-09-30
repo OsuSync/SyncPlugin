338b03f2e64c8eb9816a9dc7aec71b7f627f356b
KedamaOvO <moe@mao-yu.net>
2018-09-30 19:44:41 +0800

    config-gui 0.1.3

--- ConfigGUI/ConfigGuiPlugin.cs
+++ ConfigGUI/ConfigGuiPlugin.cs
@@ -17,7 +17,7 @@ namespace ConfigGUI
     {
         public const string PLUGIN_NAME="ConfigGUI";
         public const string PLUGIN_AUTHOR = "KedamaOvO";
-        public const string PLGUIN_VERSION = "0.1.2";
+        public const string PLGUIN_VERSION = "0.1.3";
 
         public ConfigurationItemFactory ItemFactory { get; } = new ConfigurationItemFactory();
 
